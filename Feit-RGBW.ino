48c4c7a33d1cf99706200a8b3c252445a7492b33
Brian Scott Simon <zutroy97@yahoo.com>
2018-04-10 22:24:34 -0500

    Better Patten control, white light dimmer

--- Feit-RGBW.ino
+++ Feit-RGBW.ino
@@ -4,19 +4,70 @@
 #define FW_VERSION "0.0.4"
 
 #include <Arduino.h>
-/*#include <CmdBuffer.hpp>
-#include <CmdCallback.hpp>
-#include <CmdParser.hpp>
+#include <Cmd.h>
+
+/*
 #define FASTLED_ESP8266_NODEMCU_PIN_ORDER
 */
 #include <FastLED.h>
-
-#define UPDATES_PER_SECOND 100
-
+#define INITIAL_WHITE_VALUE 0
 #define NUM_LEDS 1
 CRGB leds[NUM_LEDS];
 
-byte patternCount = 0;
+byte transistionDelay = 10; // MS to wait before setting next gradient
+uint16_t hangDelay = 0;  // MS to wait at each "hang stop"
+byte hangStepsLimit = 16; // Number of gradients to skip before waiting at next hangTime 
+char buffer[30];
+
+// Custom Patterns
+extern CRGBPalette16 myRedWhiteBluePalette;
+extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
+
+typedef struct
+{
+    //char* name;
+    char shortcut;
+} Pattern;
+
+// Names of Patterns
+const char STR_RAINBOW[] PROGMEM = "Rainbow";
+const char STR_RAINBOW_STRIPE[] PROGMEM = "Rainbow Stripe";
+const char STR_HEAT[] PROGMEM = "Heat";
+const char STR_CLOUD[] PROGMEM = "Cloud";
+const char STR_PARTY[] PROGMEM = "Party";
+const char STR_LAVA[] PROGMEM = "Lava";
+const char STR_RED_WHITE_BLUE[] PROGMEM = "Red White Blue";
+
+#define NUMBER_PATTERNS 7
+Pattern patterns[NUMBER_PATTERNS] = {
+    { 'a'},
+    {'b'},
+    {'c'},
+    {'d'},
+    {'f'},
+    {'g'},
+    {'h'}
+};
+
+CRGBPalette16 palettes[NUMBER_PATTERNS] = {
+    RainbowColors_p, 
+    RainbowStripesColors_p,
+    HeatColors_p,
+    CloudColors_p,
+    PartyColors_p,
+    LavaColors_p,
+    myRedWhiteBluePalette_p
+};
+
+const char* const patternNames[NUMBER_PATTERNS] PROGMEM = {
+    STR_RAINBOW,
+    STR_RAINBOW_STRIPE,
+    STR_HEAT,
+    STR_CLOUD,
+    STR_PARTY,
+    STR_LAVA,
+    STR_RED_WHITE_BLUE
+};
 
 /*
 // LED  ESP-8266
@@ -25,18 +76,15 @@ byte patternCount = 0;
 #define BLUEPIN 14
 #define DATA_PIN 4
 */
-// LED  Nano
+// LED  Arduino Nano 
 #define REDPIN 9 //13 // D9
 #define GREENPIN 10 //14 // D10 
 #define BLUEPIN 11 //15 // D11
+#define WHITEPIN 6 // D6
 #define DATA_PIN 4
+
 CRGBPalette16 currentPalette;
 TBlendType    currentBlending;
-
-extern CRGBPalette16 myRedWhiteBluePalette;
-extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
-
-//CmdCallback_P<3> cmdCallback;
 
 void show()
 {
@@ -46,181 +94,165 @@ void show()
   analogWrite(GREENPIN, rgb->g );
   analogWrite(BLUEPIN,  rgb->b );
 }
-/*
-void functNext(CmdParser *myParser)
-{
-    patternCount++;
-    ChangePaletteOnKeypress(patternCount);
-}
-*/
+
 void setup() {
 
-  delay(1000);
-  Serial.begin(115200);
-  
-  pinMode(REDPIN, OUTPUT); // RED
-  pinMode(GREENPIN, OUTPUT); // GREEN
-  pinMode(BLUEPIN, OUTPUT); // BLUE
-//  pinMode(D3, OUTPUT); // WHITE
-  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
-  currentPalette = RainbowColors_p;
-  currentBlending = LINEARBLEND;
+    delay(100);
+    Serial.begin(115200);
+    cmdInit(&Serial);
+    
+    pinMode(REDPIN, OUTPUT); // RED
+    pinMode(GREENPIN, OUTPUT); // GREEN
+    pinMode(BLUEPIN, OUTPUT); // BLUE
+    pinMode(WHITEPIN, OUTPUT); // WHITE
+    FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+    currentPalette = RainbowColors_p;
+    currentBlending = LINEARBLEND;
 
-  //cmdCallback.addCmd(PSTR("n"), &functNext);
-  Serial.println("Setup Complete");
+    cmdAdd( "speed", speed);
+    cmdAdd("pattern", pattern);
+    cmdAdd("help", help);
+    cmdAdd("hangtime", hangTime);
+    cmdAdd("white", white);
+    Serial.println(F("READY:"));
+    ChangePalette('a');
+    analogWrite(WHITEPIN, INITIAL_WHITE_VALUE);
 }
 
+void white(int arg_cnt, char **args)
+{
+    static byte whiteValue = INITIAL_WHITE_VALUE;
+    Stream *s = cmdGetStream();
+    switch(arg_cnt)
+    {
+        case 1:
+            s->println(whiteValue);
+            break;
+        case 2:
+            whiteValue = constrain(cmdStr2Num(args[1], 10), 0, 100);
+            whiteValue = map(whiteValue, 0, 100, 0, 255);
+            analogWrite(WHITEPIN, whiteValue);
+            break;
+    }
+}
+void hangTime(int arg_cnt, char **args)
+{
+    Stream *s = cmdGetStream();
+    switch(arg_cnt)
+    {
+        case 1:
+            s->println(hangDelay);
+            break;
+        case 2:
+            hangDelay = cmdStr2Num(args[1], 10);
+            break;
+    }
+}
+void help(int arg_cnt, char **args)
+{
+    for (byte i = 0; i< NUMBER_PATTERNS; i++)
+    {
+        Serial.print(i);Serial.print(F(" - "));
+        Serial.print(patterns[i].shortcut);Serial.print(F(" "));
+        strcpy_P(buffer, (char*) pgm_read_word(&( patternNames[i] )));
+        Serial.println(buffer);
+    }
+}
+void pattern(int arg_cnt, char **args)
+{
+    Stream *s = cmdGetStream();
+    switch(arg_cnt)
+    {
+        //case 1:
+        //s->println(patterns)
+        case 2:
+            ChangePalette(args[1][0]);
+            break;
+    }
+}
 
+void speed(int arg_cnt, char **args)
+{
+    Stream *s = cmdGetStream();
+    switch(arg_cnt)
+    {
+        case 1:
+            s->println(transistionDelay);
+            break;
+        case 2:
+            transistionDelay = cmdStr2Num(args[1], 10);
+            break;
+    }
+}
 
-int bytes2Read = 0;
+//int bytes2Read = 0;
 void loop() {
-    static uint8_t startIndex = 0;
+    static uint8_t gradientIndex = 0; // Where in the gradient are we?
+    static uint8_t transistionTimer = transistionDelay; // Counts # of loops
+    static uint16_t hangTimeTimer = hangDelay;
+    //static uint8_t hangStepIndex = 0;
 
-    //cmdCallback.loopCmdProcessing(&myParser, &myBuffer, &Serial);
-    
-    bytes2Read = Serial.available();
-    if (bytes2Read > 0)
+    cmdPoll();
+    EVERY_N_MILLISECONDS(1)
     {
-        while (bytes2Read-- > 0)
+        //Serial.print("HTT:"); Serial.println(hangTimeTimer);
+        if (hangTimeTimer == 0)
         {
-            Serial.read();
+            //Serial.print("TT:"); Serial.println(transistionTimer);
+            if (transistionTimer == 0)
+            {
+                //Serial.print("GI:"); Serial.println(gradientIndex);
+                FillLEDsFromPaletteColors(gradientIndex++); 
+                show();
+                transistionTimer = transistionDelay;
+                if (gradientIndex % hangStepsLimit == 0)
+                {
+                    hangTimeTimer = hangDelay;
+                    transistionTimer = transistionDelay;
+                }
+            }else{
+                transistionTimer--;
+            }
+        }else{
+            hangTimeTimer--;
         }
-        patternCount++;
-        ChangePaletteOnKeypress(patternCount);
     }
-    //ChangePalettePeriodically();
-    
-    EVERY_N_MILLISECONDS(100)
-    {
-        FillLEDsFromPaletteColors(++startIndex); /* motion speed */
-        show();
-    }
-    
-
-    //FastLED.delay(1000 / UPDATES_PER_SECOND);  
 }
 
 void FillLEDsFromPaletteColors( uint8_t colorIndex)
 {
-    uint8_t brightness = 255;
-    
-    for( int i = 0; i < NUM_LEDS; i++) {
-        leds[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
-        colorIndex += 3;
-    }
+    leds[0] = ColorFromPalette(currentPalette, colorIndex, 255, currentBlending);
 }
 
-void ChangePaletteOnKeypress(byte pattern)
+void ChangePalette(char c)
 {
-    byte oldPattern = 255;
-    if (pattern == oldPattern) return;
-    oldPattern = pattern;
-    Serial.print("pattern % 10 = ");
-    Serial.println(oldPattern % 11);
-
-    switch(oldPattern % 11)
+    for (byte i = 0; i< NUMBER_PATTERNS; i++)
     {
-        case 0:
-            Serial.println("RainbowColors Blend");
-            currentPalette = RainbowColors_p;
-            currentBlending = LINEARBLEND;
-            break;
-        case 1:
-            Serial.println("RainbowStripeColors NoBlend");
-            currentPalette = RainbowStripeColors_p;
-            currentBlending = NOBLEND;
-            break;    
-        case 2:
-            Serial.println("RainbowStripeColors Blend");
-            currentPalette = RainbowStripeColors_p;
-            currentBlending = LINEARBLEND;
-            break;         
-        case 3:
-            Serial.println("SetupPurpleAndGreenPalette Blend");
-            SetupPurpleAndGreenPalette();
-            currentBlending = LINEARBLEND;
-            break;
-        case 4:
-            Serial.println("SetupTotallyRandomPalette Blend");
-            SetupTotallyRandomPalette();
-            currentBlending = LINEARBLEND;
-            break;
-        case 5:
-            Serial.println("SetupBlackAndWhiteStripedPalette NoBlend");
-            SetupBlackAndWhiteStripedPalette();
-            currentBlending = NOBLEND;
-            break;
-        case 6:
-            Serial.println("SetupBlackAndWhiteStripedPalette Blend");
-            SetupBlackAndWhiteStripedPalette();
-            currentBlending = LINEARBLEND;
-            break;   
-        case 7:
-            Serial.println("CloudColors Blend");
-            currentPalette = CloudColors_p;
-            currentBlending = LINEARBLEND;
-            break;  
-        case 8:
-            Serial.println("PartyColors Blend");
-            currentPalette = PartyColors_p;
-            currentBlending = LINEARBLEND;
-            break;     
-        case 9:
-            Serial.println("myRedWhiteBluePalette NoBlend");
-            currentPalette = myRedWhiteBluePalette_p;
-            currentBlending = NOBLEND;
-            break;  
-        case 10:
-            Serial.println("myRedWhiteBluePalette Blend");
-            currentPalette = myRedWhiteBluePalette_p;
-            currentBlending = LINEARBLEND;
-            break;                                                           
+        if (patterns[i].shortcut == c)
+        {
+            currentPalette = palettes[i];
+            return;
+        }
     }
-}
-// There are several different palettes of colors demonstrated here.
-//
-// FastLED provides several 'preset' palettes: RainbowColors_p, RainbowStripeColors_p,
-// OceanColors_p, CloudColors_p, LavaColors_p, ForestColors_p, and PartyColors_p.
-//
-// Additionally, you can manually define your own color palettes, or you can write
-// code that creates color palettes on the fly.  All are shown here.
-
-// This function fills the palette with totally random colors.
-void SetupTotallyRandomPalette()
-{
-    for( int i = 0; i < 16; i++) {
-        currentPalette[i] = CHSV( random8(), 255, random8());
+    switch(c)
+    {
+        case '-':
+            //speedLimit--;
+            //Serial.print("SpeedLimit = ");Serial.println(speedLimit);
+            break; 
+        case '+':
+            //speedLimit++;
+            //Serial.print("SpeedLimit = ");Serial.println(speedLimit);
+            break;                          
+        case ' ':
+            //Serial.println("Next Color");
+            //speedLimit += 16;
+            //Serial.print("SpeedLimit = ");Serial.println(speedLimit);
+            break;
+        default:
+            Serial.println("Unknown Command");
+            break;
     }
-}
-
-// This function sets up a palette of black and white stripes,
-// using code.  Since the palette is effectively an array of
-// sixteen CRGB colors, the various fill_* functions can be used
-// to set them up.
-void SetupBlackAndWhiteStripedPalette()
-{
-    // 'black out' all 16 palette entries...
-    fill_solid( currentPalette, 16, CRGB::Black);
-    // and set every fourth one to white.
-    currentPalette[0] = CRGB::White;
-    currentPalette[4] = CRGB::White;
-    currentPalette[8] = CRGB::White;
-    currentPalette[12] = CRGB::White;
-    
-}
-
-// This function sets up a palette of purple and green stripes.
-void SetupPurpleAndGreenPalette()
-{
-    CRGB purple = CHSV( HUE_PURPLE, 255, 255);
-    CRGB green  = CHSV( HUE_GREEN, 255, 255);
-    CRGB black  = CRGB::Black;
-    
-    currentPalette = CRGBPalette16(
-                                   green,  green,  black,  black,
-                                   purple, purple, black,  black,
-                                   green,  green,  black,  black,
-                                   purple, purple, black,  black );
 }
 
 
